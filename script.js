e903bcf7bfb3a6c3e64a8dfba33f6260fc5a6da1
vtdt <127406000+vtdt@users.noreply.github.com>
2023-09-08 11:17:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const text1 = ["visu vasaru es stradaju uz c++ projekta saucams Weave cs2 (aka cheat for cs2)", "novirzoties no \"gamehacking\" es ari iemacijos daudz jauna par trigonometriju un parejo", "beidzot ar pedejo vasaras menesi es bikit satupiju un papisu visu naudu kas ir bedigi bet nav letali"];
+const text2 = ["del ta es vispar nestradaju ar html css un js visu so laiku, bet man ir laba atmina ;)", "izradas ka trigonometrija nav tik gruta un isteniba ir loti viegla", "un protams del ta ka es papisu visu naudu man nav ko est :<\nps: paypal(denmenziedu@gmail.com)"];
+
+let main_theme = document.getElementById("oao")
+let set1 = document.getElementById("text1");
+let set2 = document.getElementById("text2");
+let set_month = document.getElementById("month_id");
+let id = 0;
+
+
+
+set1.innerText = text1[id];
+set2.innerText = text2[id];
+set_month.innerText = id+1+": menesis";
+
+function check_main(id){
+    if(id == 0){
+        main_theme.innerText = "MAIN THEME";
+    }else{
+        main_theme.innerText = "";
+    }
+}
+
+
+function next_page(){
+    id += 1;
+    if(id > 2){
+        id = 2;
+    }
+    check_main(id);
+    set1.innerText = text1[id];
+    set2.innerText = text2[id];
+    set_month.innerText = id+1+": menesis";
+}
+function previous_page(){
+    id -= 1;
+    if(id < 0){
+        id = 0;
+    }
+    check_main(id);
+    set1.innerText = text1[id];
+    set2.innerText = text2[id];
+    set_month.innerText = id+1+": menesis";
+}
+
